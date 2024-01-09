3c4c7d1157d985cbd3c9951aef1aa24406b084ce
Rui Ma <535657150@qq.com>
2024-01-09 11:25:46 +0800

    test syncer darwin amd64

--- syncer-darwin.rb
+++ syncer-darwin.rb
@@ -1,4 +1,4 @@
-class SyncerDarwinAmd64 < Formula
+class SyncerDarwin < Formula
   desc "A multifunctional sync tool that supports docker registry, git repository, etc."
   homepage "https://github.com/MR5356/syncer"
   url "https://github.com/MR5356/syncer/releases/download/v1.0.0-release_build.81f53d4/syncer-darwin-amd64-build.81f53d4.tar.gz"
