bd303bb00828c4dcf9d418879a9ec49586166baf
Rui Ma <535657150@qq.com>
2024-01-09 09:54:56 +0800

    test syncer

--- syncer.rb
+++ syncer.rb
@@ -4,7 +4,7 @@ class Syncer < Formula
   license "Apache-2.0"
   version "1.0.0"
   url "https://github.com/MR5356/syncer/archive/refs/tags/v1.0.0-release_build.81f53d4.tar.gz"
-  sha256 "f012dfa0c5f14eb5c3d576b928a3624108a9d1b06627cad27fc45e5addc5b0a0"
+  sha256 "e8252140e5f051d2bddebcd38572bb0a791d37e64734c952c3e8386b789835b1"
 
   depends_on "go" => :build
 
