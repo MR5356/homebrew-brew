7d48eb5de6eb085718fca0eb585782a5891754ca
Rui Ma <535657150@qq.com>
2024-01-09 09:52:53 +0800

    test syncer

--- syncer.rb
+++ syncer.rb
@@ -3,11 +3,14 @@ class Syncer < Formula
   homepage "https://github.com/MR5356/syncer"
   license "Apache-2.0"
   version "1.0.0"
-  url "https://github.com/MR5356/syncer/releases/download/v1.0.0-release_build.81f53d4/syncer-darwin-arm64-build.81f53d4.tar.gz"
+  url "https://github.com/MR5356/syncer/archive/refs/tags/v1.0.0-release_build.81f53d4.tar.gz"
   sha256 "f012dfa0c5f14eb5c3d576b928a3624108a9d1b06627cad27fc45e5addc5b0a0"
 
+  depends_on "go" => :build
+
   def install
-    bin.install "syncer" => "syncer"
+    ldflags = "-s -w -X github.com/MR5356/syncer/pkg/version.Version=#{version}"
+    system "go", "build", ldflags, "-o", bin/"syncer"
   end
 
   test do
